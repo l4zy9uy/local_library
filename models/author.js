215974562efcdbcf03b4fd7c2f4585d676416fba
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-05-24 14:22:36 +0700

    add details about genre, book, author and bookinstance

--- models/author.js
+++ models/author.js
@@ -17,7 +17,6 @@ AuthorSchema.virtual("name").get(function () {
     if (this.first_name && this.family_name) {
         fullname = `${this.family_name}, ${this.first_name}`;
     }
-
     return fullname;
 });
 
--- models/genre.js
+++ models/genre.js
@@ -6,7 +6,8 @@ const GenreSchema = new Schema({
     name: {type: String, minLength: 3, maxLength: 100, required: true}
 });
 
-GenreSchema.virtual("url").get(() => {
+GenreSchema.virtual("url").get(function () {
+    console.log(`/catalog/genre/${this._id}`);
     return `/catalog/genre/${this._id}`
 });
 
